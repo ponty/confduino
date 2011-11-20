96c52e296be77d4c2656fc12dcfdcdc20e1e1855
ponty <titi@pince.(none)>
2011-11-20 17:20:47 +0100

    set maximum_size

--- confduino/examples/atmega88.py
+++ confduino/examples/atmega88.py
@@ -9,6 +9,7 @@ def install(id='atmega88', mcu='atmega88', f_cpu=20000000, upload='usbasp', core
     board.name = '{mcu}@{f_cpu} programmer:{upload}'.format(mcu=mcu,f_cpu=f_cpu,upload=upload)
     
     board.upload.using = upload
+    board.upload.maximum_size = 8*1024
     
     board.build.mcu = mcu
     board.build.f_cpu = str(f_cpu) + 'L'
