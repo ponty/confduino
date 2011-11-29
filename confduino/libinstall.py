44e5c10acd03f8251377b292900150ec20400e86
ponty <titi@pince.(none)>
2011-11-29 23:24:16 +0100

    improved install

--- confduino/libinstall.py
+++ confduino/libinstall.py
@@ -1,28 +1,100 @@
 from confduino.liblist import libraries_dir
-from confduino.util import tmpdir, download, clean_dir
+from confduino.util import tmpdir, download, clean_dir, ConfduinoError
 from entrypoint2 import entrypoint
 from path import path
 from pyunpack import Archive
+import decotrace
 import logging
 
 log = logging.getLogger(__name__)
+decotrace.traced = decotrace.TraceContext(log.debug)
 
- 
+@decotrace.traced 
+def noexample(x):
+    while 1:
+        f = x.next()
+        if 'example' not in f.lower():
+            yield f
+            
+@decotrace.traced 
+def create_name(root):
+    header_only = len(list(noexample(root.walkfiles('*.cpp')))) == 0
+#    libname = None
+    goodh = []
+    for h in root.files('*.h'):
+        cpp = h.stripext() + '.cpp'
+        if  header_only or cpp.exists():
+            goodh += [h]
+    assert len(goodh) > 0
+    log.debug('candidate headers:%s' % goodh)
+
+    hchoosen = None
+    if len(goodh) == 1:
+        hchoosen = goodh[0]
+    else:
+        for h in goodh:
+            n = h.namebase
+            if n in str(root):
+                hchoosen=h
+        if not hchoosen:
+            hchoosen = goodh[0]
+            
+    log.debug('choosing:%s' % hchoosen)
+        
+#            assert not libname
+    libname = hchoosen.namebase
+    return libname
+
+@decotrace.traced 
+def rename_root(root):
+    name = create_name(root)
+    log.debug('lib has no own dir')
+    root.rename(root.parent / name)
+    root = root.parent / name
+    return root
+
+@decotrace.traced 
+def fix_libdir(lib_dir):
+    allh = lib_dir.files('*.h')
+    if len(allh) == 1:
+        x = lib_dir.parent / allh[0].namebase
+        lib_dir.rename(x)
+        lib_dir = x
+    return lib_dir
+
+@decotrace.traced 
 def find_lib_dir(root):
     '''search for lib dir under root'''
     root = path(root)
     log.debug('files in dir:' + root)
     for x in root.walkfiles():
         log.debug('  ' + x)
-    def noexample(x):
-        while 1:
-            f = x.next()
-            if 'example' not in f.lower():
-                yield f
+                
+        
+    # only 1 dir in root? (example: github)
+    if not len(root.files()) and len(root.dirs()) == 1:
+        log.debug('go inside root')
+        root = root.dirs()[0]
+        
+    if  len(root.files('keywords.txt')):
+        root = rename_root(root)
+        return root, root
+    
+    keywords = list(root.walkfiles('keywords.txt'))
+    if len(keywords):
+        assert len(keywords) == 1
+        lib_dir = keywords[0].parent
+        lib_dir = fix_libdir(lib_dir)
+        return root, lib_dir
+        
 
     header_only = len(list(noexample(root.walkfiles('*.cpp')))) == 0
+    log.debug('header_only:%s' % header_only)
     lib_dir = None
-    for h in noexample(root.walkfiles('*.h')):
+    
+    headers = list(noexample(root.walkfiles('*.h')))
+    
+    for h in headers:
         cpp = h.stripext() + '.cpp'
         if  (header_only or cpp.exists()) and h.parent.name.lower() == h.namebase.lower():
             assert not lib_dir
@@ -30,18 +102,27 @@ def find_lib_dir(root):
             log.debug('found lib:' + lib_dir)  
     
     if not lib_dir:
+        if len(headers)==1 and len(list(root.files('*.h')))==0:
+            log.debug('only 1 header, not in root')
+            lib_dir = headers[0].parent
+            lib_dir = rename_root(lib_dir)
+            
+    if not lib_dir:
         # xxx.cpp and xxx.h in root? -> rename root dir
-        for h in root.files('*.h'):
-            cpp = h.stripext() + '.cpp'
-            if  header_only or cpp.exists():
-                assert not lib_dir
-                log.debug('lib has no own dir')
-                root.rename(root.parent / h.namebase)
-                root = lib_dir = root.parent / h.namebase
+        root = rename_root(root)
+        return root, root
+#        for h in root.files('*.h'):
+#            cpp = h.stripext() + '.cpp'
+#            if  header_only or cpp.exists():
+#                assert not lib_dir
+#                log.debug('lib has no own dir')
+#                root.rename(root.parent / h.namebase)
+#                root = lib_dir = root.parent / h.namebase
     assert lib_dir
     return root, lib_dir
     
     
+@decotrace.traced 
 def move_examples(root, lib_dir):
     '''find examples not under lib dir, and move into ``examples``
     '''
@@ -57,6 +138,7 @@ def move_examples(root, lib_dir):
             d.makedirs()
             x.move(d)
 
+@decotrace.traced 
 def fix_examples_dir(lib_dir):
     '''rename examples dir to ``examples``
     '''
@@ -84,21 +166,18 @@ def install_lib(url, replace_existing=False):
     fix_examples_dir(src_dlib)
     
     targ_dlib = libraries_dir() / src_dlib.name
-    doaction = 0
     if targ_dlib.exists():
         log.debug('library already exists:' + targ_dlib)
         if replace_existing:
             log.debug('remove %s' % (targ_dlib))
             targ_dlib.rmtree()
-            doaction = 1
-    else:
-        doaction = 1
+        else:
+            raise ConfduinoError('library already exists:' + targ_dlib)
         
-    if doaction:
-        log.debug('move %s -> %s' % (src_dlib, targ_dlib))
-        src_dlib.move(targ_dlib) 
-        
-        libraries_dir().copymode(targ_dlib)       
-        for x in targ_dlib.walk():
-            libraries_dir().copymode(x)
-            
+    log.debug('move %s -> %s' % (src_dlib, targ_dlib))
+    src_dlib.move(targ_dlib) 
+    
+    libraries_dir().copymode(targ_dlib)       
+    for x in targ_dlib.walk():
+        libraries_dir().copymode(x)
+    return targ_dlib.name       
--- confduino/liblist.py
+++ confduino/liblist.py
@@ -4,15 +4,58 @@ import logging
 
 log = logging.getLogger(__name__)
 
+def nocase_subitem(dir, name):
+    for x in dir.listdir():
+        if x.name.lower() == name.lower():
+            return x
+    return dir / name
+
 def libraries_dir():
-    'return library root path'
+    '''return library root path
+    
+    $ARDUINO/libraries
+    '''
     x = arduino_path() / 'libraries'
     assert x.exists(), x
     return x
 
 def libraries():
     'return installed library names'
-    ls = libraries_dir().listdir()
+    ls = libraries_dir().dirs()
+    ls = [x.name for x in ls]
+    ls.sort()
+    return ls
+
+def lib_dir(lib):
+    '''return library directory
+    
+    $ARDUINO/libraries/$LIB
+    '''
+    return libraries_dir() / lib
+
+def lib_examples_dir(lib):
+    '''return library examples directory
+    
+    $ARDUINO/libraries/$LIB/examples
+    '''
+    return nocase_subitem(lib_dir(lib) , 'examples')
+
+def lib_example_dir(lib, example):
+    '''return library example directory
+    
+    $ARDUINO/libraries/$LIB/examples/$EXAMPLE
+    '''
+    return lib_examples_dir(lib) / example
+    
+def lib_examples(lib):
+    '''return library examples 
+    
+    EXAMPLE1,EXAMPLE2,..
+    '''
+    d = lib_examples_dir(lib)
+    if not d.exists():
+        return []
+    ls = d.dirs()
     ls = [x.name for x in ls]
     ls.sort()
     return ls
