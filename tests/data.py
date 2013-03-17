8d012eed5c86609bf0c76451cff70300c426a9f9
ponty <ponty@home>
2013-03-17 19:56:34 +0100

    update tests

--- tests/data.py
+++ tests/data.py
@@ -4,7 +4,7 @@ TEST_ARDUINO_INSTALLATIONS = [
     Bunch(path='~/opt/arduino-0022', ver='0022', iver=22),
     Bunch(path='~/opt/arduino-1.0', ver='1.0', iver=100),
     Bunch(path='~/opt/arduino-1.0.3', ver='1.0.3', iver=103),
-    Bunch(path='/usr/share/arduino', ver='1.0', iver=100),
+    Bunch(path='/usr/share/arduino', ver='1.0.1', iver=101),
 ]
 
 TEST_ARDUINO_PATHS = [x.path for x in TEST_ARDUINO_INSTALLATIONS]
